bf4af4852117f3284073a65dbe8d00c231c77c47
Shriya Minocha <sm997@dcc-login-02.oit.duke.edu>
2024-06-24 22:57:56 -0400

    QC Alignment and Coverage Plot

---
+++ src/plotcov.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+module load Java/1.8.0_60
+module load samtools/1.9
+samtools index $1
+samtools index $2
+samtools index $3
+samtools index $4
+plotCoverage -b $1 $2 $3 $4 -o coverage2.png
---
+++ src/qc_alignment.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#SBATCH --mem=16G
+
+sample=$1
+module load Java/1.8.0_60
+module load samtools/1.9
+
+#marking duplicates
+echo "Processing sample: $sample"
+java -jar -Xmx7g /hpc/group/wraylab/kp275/team_heliconius/picard.jar MarkDuplicates -INPUT $sample -OUTPUT ${sample}.dedup.bam -METRICS_FILE metrics.txt -VALIDATION_STRINGENCY LENIENT
+
+
+file="${sample}.dedup.bam"
+
+if [ ! -f "$file" ]; then
+    echo "Output file not found: $file"
+    exit 1
+fi
+
+echo "Output file: $file"
+
+#generating bam statistics
+samtools flagstat $file
